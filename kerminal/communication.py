16cf2d7ccbeb9b84365dd2c37a3f138856d35378
SavinaRoja <pablo.barton@gmail.com>
2014-09-16 22:18:39 -0400

    using OrderedSet instead of set for data_log_vars

--- kerminal/communication.py
+++ kerminal/communication.py
@@ -3,6 +3,7 @@
 from .telemachus_api import plotables
 
 import asyncio
+import collections
 import json
 import logging
 import threading
@@ -21,12 +22,71 @@ LIVE_DATA = {k: 0 for k in plotables}
 global MSG_QUEUE
 MSG_QUEUE = queue.Queue()
 
+
+class OrderedSet(collections.MutableSet):
+
+    def __init__(self, iterable=None):
+        self.end = end = []
+        end += [None, end, end]         # sentinel node for doubly linked list
+        self.map = {}                   # key --> [key, prev, next]
+        if iterable is not None:
+            self |= iterable
+
+    def __len__(self):
+        return len(self.map)
+
+    def __contains__(self, key):
+        return key in self.map
+
+    def add(self, key):
+        if key not in self.map:
+            end = self.end
+            curr = end[1]
+            curr[2] = end[1] = self.map[key] = [key, curr, end]
+
+    def discard(self, key):
+        if key in self.map:
+            key, prev, next = self.map.pop(key)
+            prev[2] = next
+            next[1] = prev
+
+    def __iter__(self):
+        end = self.end
+        curr = end[2]
+        while curr is not end:
+            yield curr[0]
+            curr = curr[2]
+
+    def __reversed__(self):
+        end = self.end
+        curr = end[1]
+        while curr is not end:
+            yield curr[0]
+            curr = curr[1]
+
+    def pop(self, last=True):
+        if not self:
+            raise KeyError('set is empty')
+        key = self.end[1][0] if last else self.end[2][0]
+        self.discard(key)
+        return key
+
+    def __repr__(self):
+        if not self:
+            return '%s()' % (self.__class__.__name__,)
+        return '%s(%r)' % (self.__class__.__name__, list(self))
+
+    def __eq__(self, other):
+        if isinstance(other, OrderedSet):
+            return len(self) == len(other) and list(self) == list(other)
+        return set(self) == set(other)
+
 #All of this LOGGING stuff is for data logs, not runtime logs
 global DATA_LOG_ON, DATA_LOG_VARS, DATA_LOG_FILE
 DATA_LOG_ON = False
-DATA_LOG_VARS = set(['t.universalTime',
-                     'v.missionTime',
-                     'sys.time'])
+DATA_LOG_VARS = OrderedSet(['t.universalTime',
+                            'v.missionTime',
+                            'sys.time'])
 DATA_LOG_FILE = 'kerminaldata.csv'
 
 
